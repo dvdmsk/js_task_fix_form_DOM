45af2a1fc4442375d590e38513627bb6a045eb93
David Moskalenko <dvdmsk21@gmail.com>
2025-02-15 15:52:47 +0200

    Add input placeholders and labels with corrected names in the form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 'use strict';
 
 // write code here
+const inputs = document.querySelectorAll('input');
+
+for (const input of inputs) {
+  input.setAttribute('placeholder', corectName(input.name));
+
+  const label = document.createElement('label');
+
+  label.setAttribute('for', input.id);
+  label.className = 'field-label';
+  label.textContent = input.placeholder;
+
+  input.before(label);
+}
+
+function corectName(str) {
+  const newStr = str
+    .split('')
+    .map((el, i) => {
+      if (i === 0) {
+        return el.toUpperCase();
+      }
+
+      if (el === el.toUpperCase() && i !== 0) {
+        return ' ' + el;
+      }
+
+      return el;
+    })
+    .join('');
+
+  return newStr;
+}
